a26f86980688e8d4c680843cf9d5c4c2e99d3372
edilberto1960 <redilberto@gmail.com>
2018-05-08 22:04:36 -0500

    Token Login Usuarios

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Rutes
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+
 
 // Concexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
@@ -13,14 +29,12 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) 
 
 });
 
-// Rutas
-app.get('/', (req, res, next) => {
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada con exito!!!'
-    });
-});
+// Listen Routes
+
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 app.listen(3000, () => {
 
